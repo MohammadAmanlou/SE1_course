e8439e61cefb4e82c7e3059a37debad4d3f2311e
MohammadAmanlou <mohammadAmanlou2@gmail.com>
2024-04-10 18:42:22 +0330

    add MEQ test + test forbidden updating MEQ

---
+++ src/test/java/ir/ramtung/tinyme/domain/MEQTest.java
@@ -0,0 +1,86 @@
+package ir.ramtung.tinyme.domain;
+
+import ir.ramtung.tinyme.config.MockedJMSTestConfig;
+import ir.ramtung.tinyme.domain.entity.*;
+import ir.ramtung.tinyme.domain.service.Matcher;
+import ir.ramtung.tinyme.domain.service.OrderHandler;
+import ir.ramtung.tinyme.messaging.EventPublisher;
+import ir.ramtung.tinyme.messaging.Message;
+import ir.ramtung.tinyme.messaging.TradeDTO;
+import ir.ramtung.tinyme.messaging.event.*;
+import ir.ramtung.tinyme.messaging.request.DeleteOrderRq;
+import ir.ramtung.tinyme.messaging.request.EnterOrderRq;
+import ir.ramtung.tinyme.repository.BrokerRepository;
+import ir.ramtung.tinyme.repository.SecurityRepository;
+import ir.ramtung.tinyme.repository.ShareholderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@Import(MockedJMSTestConfig.class)
+@DirtiesContext
+public class MEQTest {
+    @Autowired
+    OrderHandler orderHandler;
+    @Autowired
+    EventPublisher eventPublisher;
+    @Autowired
+    SecurityRepository securityRepository;
+    @Autowired
+    BrokerRepository brokerRepository;
+    @Autowired
+    ShareholderRepository shareholderRepository;
+    private Security security;
+    private Shareholder shareholder;
+    private Broker broker1;
+    private Broker broker2;
+    private Broker broker3;
+
+    @BeforeEach
+    void setup() {
+        securityRepository.clear();
+        brokerRepository.clear();
+        shareholderRepository.clear();
+
+        security = Security.builder().isin("ABC").build();
+        securityRepository.addSecurity(security);
+
+        shareholder = Shareholder.builder().build();
+        shareholder.incPosition(security, 100_000);
+        shareholderRepository.addShareholder(shareholder);
+
+        broker1 = Broker.builder().brokerId(1).build();
+        broker2 = Broker.builder().brokerId(2).build();
+        broker3 = Broker.builder().brokerId(2).build();
+        brokerRepository.addBroker(broker1);
+        brokerRepository.addBroker(broker2);
+        brokerRepository.addBroker(broker3);
+    }
+
+    @Test
+    void updating_MEQ() {
+        Broker broker1 = Broker.builder().brokerId(1).credit(100_000).build();
+        brokerRepository.addBroker(broker1);
+        Order order = new Order(100, security, Side.BUY, 30, 500, broker1, shareholder,0);
+        security.getOrderBook().enqueue(order);
+
+        orderHandler.handleEnterOrder(EnterOrderRq.createUpdateOrderRq(1, "ABC", 100, LocalDateTime.now(), Side.BUY, 30, 550, broker1.getBrokerId(), shareholder.getShareholderId(), 0,20));
+
+        assertThat(broker1.getCredit()).isEqualTo(100_000);
+        verify(eventPublisher).publish(new OrderRejectedEvent(1, 100, List.of(Message.CAN_NOT_UPDATE_ORDER_MINIMUM_EXECUTION_QUANTITY)));
+    }
+
+
+}
