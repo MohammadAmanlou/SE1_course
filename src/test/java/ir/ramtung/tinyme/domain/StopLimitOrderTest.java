530e9110a42225ebea84911cd33922a0d78676b0
thegreatgolboo <thegreatgolboo@golboo.golboo.org>
2024-04-28 18:56:08 +0330

    new debuged test2

--- src/test/java/ir/ramtung/tinyme/domain/StopLimitOrderTest.java
+++ src/test/java/ir/ramtung/tinyme/domain/StopLimitOrderTest.java
@@ -365,16 +365,33 @@ public class StopLimitOrderTest {
     }
 
     @Test
-    void update_quantity_for_inactive_buy_stop_limit_order_successfully_done(){
-        Broker broker1 = Broker.builder().brokerId(1).credit(100_000).build();
-        brokerRepository.addBroker(broker1);
+    void update_quantity_for_inactive_buy_stop_limit_order_successfully_done() {
 
-        Order stopLimitOrder = new StopLimitOrder(100, security, Side.BUY, 10, 500, broker1, shareholder,   10 , 0);
-        int prevQuantity = stopLimitOrder.getQuantity();
-        orderHandler.handleEnterOrder(EnterOrderRq.createUpdateOrderRq(1, "ABC", 100, LocalDateTime.now(), Side.BUY, 50, 500,
-                broker1.getBrokerId(), shareholder.getShareholderId(), 0, 0 , 10));
+        Broker broker1 = Broker.builder().brokerId(10).credit(100_000).build();
+        Broker broker2 = Broker.builder().brokerId(20).credit(100_000).build();
+        Broker broker3 = Broker.builder().brokerId(30).credit(520_000).build();
+        List.of(broker1, broker2, broker3).forEach(b -> brokerRepository.addBroker(b));
 
-        assertThat(broker1.getCredit()).isEqualTo(100_000 - Math.abs(stopLimitOrder.getQuantity() - prevQuantity) * stopLimitOrder.getPrice() );
+        Order matchingSellOrder1 = new Order(100, security, Side.BUY, 30, 500, broker1, shareholder,0);
+        Order matchingSellOrder2 = new Order(110, security, Side.SELL, 20, 400, broker1, shareholder,0);
+        security.getOrderBook().enqueue(matchingSellOrder1);
+        security.getOrderBook().enqueue(matchingSellOrder2);
+
+        orderHandler.handleEnterOrder(EnterOrderRq.createNewOrderRq(1, "ABC", 200, LocalDateTime.now(),
+                Side.BUY, 10, 700, broker2.getBrokerId(), shareholder.getShareholderId(),
+                0 , 0 , 200));
+
+        orderHandler.handleEnterOrder(EnterOrderRq.createNewOrderRq(2, "ABC", 300, LocalDateTime.now(),
+                Side.BUY, 10, 600, broker3.getBrokerId(), shareholder.getShareholderId(),
+                0 , 0 , 300));
+
+        orderHandler.handleEnterOrder(EnterOrderRq.createUpdateOrderRq(3, "ABC", 300, LocalDateTime.now(), Side.BUY, 20, 600,
+                broker1.getBrokerId(), shareholder.getShareholderId(), 0, 0 , 300));
+
+        verify(eventPublisher.get(0)).publish(new OrderUpdatedEvent(3,300));
+
+        assertThat(security.getOrderBook().findInActiveByOrderId(Side.BUY , 300, 300, false).getQuantity()).isEqualTo(20);
+
     }
 
     @Test
